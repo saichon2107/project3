a0272d5e68acdc6e7db5e0a2f6ae55b3a743acac
saichon <saichon2107>
2021-06-26 11:08:27 +0700

    ex3.php ex4.php ex5.php commit

---
+++ Ex5.php
@@ -0,0 +1,17 @@
+<?php
+//โปรแกรมคำนวนราคาสินค้า
+
+$price =20;  // ราคาสินค้า
+$quantiry =30; // จำนวน
+$total = $price * $quantiry;  // ราคารวมคิดโดยนำ ราคา x จำนวน
+ // ภาษี 7%
+
+ $vat= $total * 0.07;
+ $totalAII =$total + $vat;
+
+ echo ("ราคาสินค้า" . $price . "จำนวน " . $quantiry . "ราคารวม " . $total);
+ echo ("<br>");
+ echo ("ภาษี 7 % = " . $vat);
+ echo ("<br>");
+ echo ("ราคาสินค้ารวมภาษี =". $totalAII . "บาท");
+ ?>
